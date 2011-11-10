bfb8585a8b2766182439c9b6b6b809f91e3fda4d
Matt Gifford <me@matthewgifford.com>
2011-11-10 09:35:54 -0800

    Changed from camera to capture api

--- scavenger/scripts/app.js
+++ scavenger/scripts/app.js
@@ -6,21 +6,21 @@ $(document).bind("mobileinit", function() {
 
 $(document).ready(function() {
   document.addEventListener("deviceready", function() {
+    
+    var captureSuccess = function(mediaFiles) {
+      navigator.notification.alert('Success!', null, 'Success');
+    }
 
-    $('#camera').click(function() {
-      navigator.camera.getPicture(
-        function(imageURI) {
-          alert(imageURI);
-        },
-        function (message) {
-          alert(message);
-        },
-        {
-          quality: 50,
-          destinationType: Camera.DestinationType.FILE_URI
-        }
-      );
-    });
+    var captureError = function(error) {
+      var msg = 'An error occurred during capture: ' + error.code;
+      navigator.notification.alert(msg, null, 'Error'); 
+    };
+
+    var captureImage = function() {
+      navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 1});
+    };
+
+    $('#camera').click(captureImage);
 
   }, false);
 });
