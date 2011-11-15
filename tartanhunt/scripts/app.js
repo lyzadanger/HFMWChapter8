847026810fcbcfa6c9d6584cb9e65df4bb9d2cbb
Lyza Danger Gardner <lyza@lyza.com>
2011-11-15 11:57:40 -0800

    Aileen's CSS integrated; app.js further simplified

--- tartanhunt/scripts/app.js
+++ tartanhunt/scripts/app.js
@@ -1,6 +1,6 @@
 (function() {
   var imageCaptureSupported = false;
-  $(document).bind("mobileinit", function() { // no transitions: for performance
+  $(document).bind("mobileinit", function() {
     $.extend($.mobile, { defaultPageTransition: 'none' });
     $.mobile.page.prototype.options.addBackBtn = true;
   });
@@ -11,14 +11,13 @@
       $('[data-rel="back"]').hide();
     }
     imageCaptureSupported = true;
-    $('.foundTartan').html('Snap Photo of Tartan!');
   }
 
   initDevice = function() {
-    if (typeof(window.localStorage == 'object')) { // Browser supports localStorage
+    if (typeof(window.localStorage == 'object')) {
       $('.foundTartan').click(tartanFound);
       $('#booths').live('pageshow', function() { refreshTartans(); });
-      refreshTartans(); // Trigger once now...
+      refreshTartans();
       addResetButton();
     }
     document.addEventListener('deviceready', initPhoneGap, false);
@@ -27,23 +26,29 @@
 
   refreshTartans = function() {
     $('ul.details').each(function() {
-      var el_id     = $(this).attr('id');
-      var tartanKey = 'found-' + el_id;
-      if (isFound(tartanKey)) {
-        $('#vendor-' + el_id).addClass('found');
-        $('[data-url*="' + el_id + '"]').addClass('found');
-        showTartanImage(tartanKey, $(this));
+      var myID      = $(this).attr('id');
+      var tartanKey = 'found-' + myID;
+      var isFound   = localStorage.getItem(tartanKey);
+      if (isFound) {
+        $('#vendor-'+ myID).add($('[data-url*="'+ myID +'"]')).addClass('found');
+        var hasPhoto = isFound != 'true' || false;
+        if (hasPhoto && !$(this).find('.tartanImage').length) {
+          var $tartanHolder = $('<p></p>').append($('<img>').attr({
+            'src'     : isFound,
+            'class'   : 'tartanImage'
+          })); 
+          $(this).append('<li data-role="list-divider">My Photo of the Tartan!</li>');
+          $('<li></li>').append($tartanHolder).appendTo($(this));
+        }
         $('#' + tartanKey).closest('li').hide();
       }
     });
     $('ul').each(function() {
-      if ($(this).data('listview')) {
-        $(this).listview('refresh');
-      }
+      if ($(this).data('listview')) { $(this).listview('refresh'); }
     });
   };
   
-  tartanFound = function(event) { // Click handler for 'found it' button
+  tartanFound = function(event) {
     var $tartanButton = $(event.currentTarget);
     var tartanKey     = $tartanButton.attr('id');
     if(imageCaptureSupported) {
@@ -57,29 +62,14 @@
       refreshTartans(); 
     }
   };
-  
-  showTartanImage = function(tartanKey, $listElement) {
-    var path      = localStorage.getItem(tartanKey);
-    if ($listElement.find('.tartanImage').length || !path || path == 'true') { return; };
-    var $tartanHolder = $('<p></p>').append($('<img>').attr({
-      'src'     : path,
-      'class'   : 'tartanImage'
-    })); 
-    var $tartanElement = $('<li></li>').append($tartanHolder);
-    $listElement.append('<li data-role="list-divider">My Photo of the Tartan!</li>');
-    $listElement.append($tartanElement);
-  };
-
   captureError = function(error) { console.log(error);  }
-  isFound = function(tartanKey) { return localStorage.getItem(tartanKey) || false; };
 
   addResetButton = function() {
     var $resetButton = $('<a></a>').attr('data-role','button').html('Start Over!');
-    $resetButton.click(resetTartans).appendTo($('#booths'));
-  };
-  
-  resetTartans = function() {
-    localStorage.clear();
-    window.location.reload();
+    $resetButton.click(function() {
+      localStorage.clear();
+      window.location.reload();
+    });
+    $resetButton.appendTo($('#booths'));
   };
 })();
