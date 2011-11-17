24d56fbf8aba224a5f87d588bfccb5645d4f3eb9
Lyza Danger Gardner <lyza@lyza.com>
2011-11-16 20:20:01 -0800

    Moving along on step 3!

--- build.sh
+++ build.sh
@@ -6,8 +6,12 @@ cp 01_phonegap_basic/01_config_xml/index.html chapter8
 cp -LR tartanhunt/images chapter8
 mkdir chapter8/scripts
 cp tartanhunt/scripts/jquery* chapter8/scripts
+cp 01_phonegap_basic/02_splash_icons/scripts/app.js chapter8/scripts
 cp -LR tartanhunt/style chapter8
 mkdir chapter8/extras
 mkdir chapter8/extras/images
+mkdir chapter8/extras/scripts
+cp 02_local_storage/scripts/done.js chapter8/extras/scripts/app.localStorage.js
 mv chapter8/images/splash* chapter8/extras/images
+
 zip -qr chapter8.zip chapter8
